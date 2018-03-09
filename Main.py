7373ba2d68eca39b94e4128d995e81d5f2744360
kierankilgannon <32903766+kierankilgannon@users.noreply.github.com>
2018-03-09 14:06:33 +0000

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import pymysql
+
+# Open database connection
+db = pymysql.connect(host='mydbinstance.cbooiucez8xp.eu-west-1.rds.amazonaws.com',user='kieransDatabase',passwd='thisismypassword',db='TESTDB')
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Drop table if it already exist using execute() method.
+cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+
+# Create table as per requirement
+sql = """CREATE TABLE EMPLOYEE (
+   FIRST_NAME  CHAR(20) NOT NULL,
+   LAST_NAME  CHAR(20),
+   AGE INT,  
+   SEX CHAR(1),
+   INCOME FLOAT )"""
+
+cursor.execute(sql)
+
+# Prepare SQL query to INSERT a record into the database.
+sql = "INSERT INTO EMPLOYEE(FIRST_NAME, \
+   LAST_NAME, AGE, SEX, INCOME) \
+   VALUES ('%s', '%s', '%d', '%c', '%d' )" % \
+   ('Mac', 'Mohan', 20, 'M', 2000)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+# disconnect from server
+db.close()
